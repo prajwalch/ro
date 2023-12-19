b5fd74ff537714633b1662697cc82fc7257779fd
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-12-20 00:13:02 +0545

    feat(main): Implement command line args
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,68 @@ mod console;
 
 use std::io::{self, Write};
 use std::net::Ipv4Addr;
-use std::thread;
+use std::process::ExitCode;
 use std::time::Duration;
+use std::{env, thread};
 
 use crate::api::ApiClient;
 
-fn main() {
+const USAGE: &str = "Usage: ro [OPTIONS]
+
+Options:
+  --scan        Scan and display all the available wifi
+  --reboot      Reboot the router
+  --reset       Reset the router
+  --help        Print this help
+";
+
+fn main() -> ExitCode {
     let mut api = match ApiClient::new(Ipv4Addr::new(192, 168, 16, 1)) {
         Ok(api) => api,
         Err(err) => {
             eprintln!("error: Failed to initilize controller, {err}");
-            return;
+            return ExitCode::FAILURE;
         }
     };
 
     if let Err(e) = api.login("admin", "admin") {
-        eprintln!("error: Failed to login, {e}")
+        eprintln!("error: Failed to login, {e}");
+        return ExitCode::FAILURE;
     };
+    let mut args = env::args();
+
+    match args.nth(1).as_deref() {
+        Some("--scan") => {
+            let wifi_list = api.scan_wifi().unwrap();
+
+            for wifi in wifi_list {
+                println!("{} \t {} \t {}", wifi.channel, wifi.ssid, wifi.signal);
+            }
+            return ExitCode::SUCCESS;
+        }
+        Some("--reboot") => {
+            api.reboot().unwrap();
+            return ExitCode::SUCCESS;
+        }
+        Some("--reset") => {
+            api.reset().unwrap();
+            return ExitCode::SUCCESS;
+        }
+        Some("--help") => {
+            println!("{USAGE}");
+            return ExitCode::SUCCESS;
+        }
+        Some(unknown) => {
+            println!("error: Unknown option, '{unknown}'");
+            println!("{USAGE}");
+            return ExitCode::FAILURE;
+        }
+        None => (),
+    }
 
     if let Ok(Some(ssid)) = api.connected_ssid() {
         println!("Connected to: {ssid}");
     }
-
     let mut stdout = io::stdout().lock();
 
     while let Ok(router_info) = api.router_info() {
@@ -38,4 +78,6 @@ fn main() {
         stdout.flush().unwrap();
         thread::sleep(Duration::from_secs(2));
     }
+
+    ExitCode::SUCCESS
 }
